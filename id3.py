d8a6ba8d5db36e3b32d38749cc5032ca6dc932c0
naigon <naigon_mm@yahoo.com.br>
2016-08-25 00:06:21 -0300

    Algumas alterações

--- id3.py
+++ id3.py
@@ -14,6 +14,7 @@ def main():
 		v_temperature = ['Hot','Mild','Cool']
 		v_humidity = ['High','Normal'] 
 		v_wind = ['Weak','Strong']
+		v_beach = ['Yes','No']#classe
 		for row in dados:
 			day = row[0]
 			outlook = row[1]
@@ -31,18 +32,26 @@ def main():
 			
 		n_exemplos = len(days) - 1
 		
-		count_y = beachs.count('Yes')
-		count_n = beachs.count('No')
+		count_y = beachs.count(v_beach[0])
+		count_n = beachs.count(v_beach[1])
 		entropia_exemplos = -count_y/n_exemplos * math.log(count_y/n_exemplos, 2) - count_n/n_exemplos * math.log(count_n/n_exemplos, 2)
 		print ('Entropia dos exemplos: ' , entropia_exemplos)
 		entropia_wind = entropia(winds, n_exemplos, beachs)
 		
 def calc_entropia(atributo_alvo, valores_atributo, n_exemplos, classes):
+	count_p = 0
+	count_n = 0
 	tam = len(valores_atributo)
 	count = list(range(tam))
+	
 	for i in range(tam):
 		count[i] = atributo_alvo.count(valores_atributo[i])
-######################################################################
+		
+	for i in atributo_alvo:
+		if atributo_alvo[i] and classes[i]=='Yes':
+			count_p = count_p + 1
+		elif: atributo_alvo[i] and classes[i]=='No':
+			count_n = count_n + 1
 	return 
 
 
