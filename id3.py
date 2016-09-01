c0cf322c6924870b55de72535ed88678c715ec5d
naigon <naigon_mm@yahoo.com.br>
2016-09-01 18:30:15 -0300

    Adicionado comentarios
    função que cria a arvore de decisao incompleta

--- id3.py
+++ id3.py
@@ -4,17 +4,22 @@ import csv, math
 def main():
 	with open('beach.csv') as f:
 		dados = csv.reader(f, delimiter=',')
+		
+		#lista com cada atributo e as ocorrencias dos respectivos rotulos
 		days = []
 		outlooks = []
 		temperatures = []
 		humiditys = []
 		winds = [] 
 		beachs = []
+		
+		#listas com os rotulos possiveis de cada atributo
 		v_outlook = ['Sunny','Overcast','Rain','','','','','','','','','','','','']
 		v_temperature = ['Hot','Mild','Cool','','','','','','','','','','','','']
 		v_humidity = ['High','Normal','Null','','','','','','','','','','',''] 
 		v_wind = ['Weak','Strong','Null','','','','','','','','','','','','']
 		v_beach = ['Yes','No','','','','','','','','','','','','','']#classe
+		
 		for row in dados:
 			day = row[0]
 			outlook = row[1]
@@ -32,7 +37,7 @@ def main():
 		
 		n_exemplos = len(days) - 1
 		tam = 3
-		
+		#listas que irao armazenar a ocorrencia de cada um dos rotulos nos atributos
 		ocorrencia_r_outlook = list(range(tam))
 		ocorrencia_r_temperature = list(range(tam))
 		ocorrencia_r_humidity = list(range(tam))
@@ -47,12 +52,13 @@ def main():
 			ocorrencia_r_beach[i] = beachs.count(v_beach[i])
 		
 		#entropia exemplos
-		count_y = beachs.count(v_beach[0])
-		count_n = beachs.count(v_beach[1])
+		count_y = beachs.count(v_beach[0])#ocorrencias de 'Yes'
+		count_n = beachs.count(v_beach[1])#ocorrencias de 'No'
 		entropia_exemplos = -ocorrencia_r_beach[0]/n_exemplos * math.log(ocorrencia_r_beach[0]/n_exemplos, 2) - ocorrencia_r_beach[1]/n_exemplos * math.log(ocorrencia_r_beach[1]/n_exemplos, 2)
 		
 		print ('\nEntropia dos Exemplos: ' , round(entropia_exemplos,6))
-		#calculo do ganho para a escolha do atributo de decisão
+		
+		#calculo do ganho para a escolha do atributo de decisão(raiz da arvore)
 		ganho_wind = ganho(entropia_exemplos, winds,v_wind, ocorrencia_r_wind, n_exemplos, beachs)
 		ganho_outlook = ganho(entropia_exemplos, outlooks, v_outlook, ocorrencia_r_outlook, n_exemplos, beachs)
 		ganho_humidity = ganho(entropia_exemplos, humiditys, v_humidity, ocorrencia_r_humidity, n_exemplos, beachs)
@@ -63,7 +69,6 @@ def main():
 		print('Ganho atributo Temperature: ', round(ganho_temperature,6))
 		print('Ganho atributo Outlook: ', round(ganho_outlook,6))
 
-
 def calc_entropia(atributo_alvo, rotulo_alvo, ocorrencia_rotulo, classes):
 	n_ocorrencias = list(range(2))
 	n_ocorrencias = ocorrencias_classe(atributo_alvo, rotulo_alvo, classes)
@@ -71,6 +76,7 @@ def calc_entropia(atributo_alvo, rotulo_alvo, ocorrencia_rotulo, classes):
 	entropia = round(entropia,6)
 	return entropia
 
+#função que faz a contagem, pra cada rotulo, quantos 'Yes' e 'No' possuem
 def ocorrencias_classe(atributo_alvo, rotulo_atributo, classes):
 	ocorrencia = list(range(2))
 	ocorrencia[0] = 0
@@ -104,6 +110,5 @@ def ganho(entropia, atributo,valores_rotulo, ocorrencias_rotulo, n_exemplos, cla
 	for i in range(laco):
 		ganho -= ocorrencias_rotulo[i]/n_exemplos * calc_entropia(atributo, valores_rotulo[i], ocorrencias_rotulo[i], classes)
 	return round(ganho,6)
-
 
 main()
